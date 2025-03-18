a285af1d26f14237bfb21d58144381a497cf31b3
youminki <dbalsrl7648@gmail.com>
2025-03-18 14:26:05 +0900

    Settting : vite.config base설정

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 import svgrPlugin from 'vite-plugin-svgr';
 
 export default defineConfig({
+  base: './',
   plugins: [
     react(),
     svgrPlugin(), // SVG를 React 컴포넌트로 사용 가능하게 함
